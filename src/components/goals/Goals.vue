b7ed02d5fda1eec30bb637a915aa8d9fc2d22fd2
Felix Wegener <felix.wegener@iem.thm.de>
2021-03-03 17:05:52 +0100

    Unsere Ziele erstellt.
    Styling fehlt.

--- src/components/goals/Goals.vue
+++ src/components/goals/Goals.vue
@@ -1,6 +1,119 @@
 <template>
   <div>
-    <p>Ziele</p>
+    <img src="../../assets/images/youth-climate-strike-aus.jpg" class="img-thumbnail" alt="Klimastreik"/>
+    <div class="container d-flex justify-content-center align-items-center">
+      <div class="text-center d-flex justify-content-center align-items-center Banner">
+        <h1 class="text-white">
+          Was wir fordern!
+        </h1>
+      </div>
+    </div>
+    <div class="d-flex flex-row flex-wrap justify-content-center">
+      <div class="container d-flex justify-content-center p-4 infoContainer">
+        <div class="row d-flex justify-content-center">
+          <h5 class="fw-bold">
+            1.Erderhitzung stoppen
+          </h5>
+          <div class="row d-flex justify-content-centers">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+              et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+      <div class="container d-flex justify-content-center p-4 infoContainer">
+        <div class="row d-flex justify-content-center">
+          <h5 class="fw-bold">
+            2. Pariser Klimaschutzabkommen einhalten
+          </h5>
+          <div class="row d-flex justify-content-centers">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+              et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+      <div class="container d-flex justify-content-center p-4 infoContainer">
+        <div class="row d-flex justify-content-center">
+          <h5 class="fw-bold">
+            3.Lorem Ipsum
+          </h5>
+          <div class="row d-flex justify-content-centers">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+              et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+      <div class="container d-flex justify-content-center p-4 infoContainer">
+        <div class="row d-flex justify-content-center">
+          <h5 class="fw-bold">
+            4.Lorem Ipsum
+          </h5>
+          <div class="row d-flex justify-content-centers">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+              et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+      <div class="container d-flex justify-content-center p-4 infoContainer">
+        <div class="row d-flex justify-content-center">
+          <h5 class="fw-bold">
+            5.Lorem Ipsum
+          </h5>
+          <div class="row d-flex justify-content-centers">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+              et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+      <div class="container d-flex justify-content-center p-4 infoContainer">
+        <div class="row d-flex justify-content-center">
+          <h5 class="fw-bold">
+            6.Lorem Ipsum
+          </h5>
+          <div class="row d-flex justify-content-centers">
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+              et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr/>
+    <div class="container d-flex justify-content-center p-4">
+      <div class="row d-flex justify-content-center">
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <ul class="list-unstyled mb-0">
+            <li>
+              <h4 class="fw-bold">DIE FORDERUNGEN</h4>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+
+          <ul class="list-unstyled">
+            <li>
+              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+            </li>
+            <li>
+              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+            </li>
+            <li>
+              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,5 +126,31 @@ export default defineComponent({
 </script>
 
 <style scoped>
+
+.Banner {
+  top: 550px;
+  height: 150px;
+  width: 800px;
+  background-color: #67bb7d;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(4px);
+  /* Note: backdrop-filter has minimal browser support */
+
+  border-radius: 49px;
+}
+
+.infoContainer {
+  width: 40%;
+  height: 300px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(4px);
+  /* Note: backdrop-filter has minimal browser support */
+
+  border-radius: 20px;
+  border-style: dashed;
+  border-color: #67bb7d;
+
+  margin: 10px;
+}
 
 </style>
