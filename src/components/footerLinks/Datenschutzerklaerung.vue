142334128f71ce981d7a6c710b63eac7600eea44
Felix Wegener <felix.wegener@iem.thm.de>
2021-03-14 17:31:30 +0100

    Datenschutz angefangen

--- src/components/footerLinks/Datenschutzerklaerung.vue
+++ src/components/footerLinks/Datenschutzerklaerung.vue
@@ -1,6 +1,40 @@
 <template>
   <div>
     <h1>Datenschutzerklärung</h1>
+    <hr/>
+    <ol class="list mb-0">
+      <li>
+        <p class="fw-bold">DATENSCHUTZ AUF EINEN BLICK</p>
+      </li>
+      <li>
+        <p class="fw-bold">ALLGEMEINE HINWEISE UND PFLICHTINFORMATIONEN</p>
+      </li>
+      <li>
+        <p class="fw-bold">DATENERFASSUNG AUF UNSERER WEBSITE</p>
+      </li>
+      <li>
+        <p class="fw-bold">SOZIALE MEDIEN</p>
+      </li>
+      <li>
+        <p class="fw-bold">NEWSLETTER</p>
+      </li>
+      <li>
+        <p class="fw-bold">PLUGINS UND TOOLS</p>
+      </li>
+      <li>
+        <p class="fw-bold">EINSATZ VON GOOGLE ANALYTICS</p>
+      </li>
+      <li>
+        <p class="fw-bold">DEMOGRAFISCHE MERKMALE BEI GOOGLE ANALYTICS</p>
+      </li>
+      <li>
+        <p class="fw-bold">GOOGLE ADWORDS CONVERSION TRACKING</p>
+      </li>
+      <li>
+        <p class="fw-bold">EINSATZ VON TWINGLE
+        </p>
+      </li>
+    </ol>
   </div>
 </template>
 
@@ -8,7 +42,7 @@
 import {defineComponent} from 'vue';
 
 export default defineComponent({
-name: "Datenschutzerklaerung"
+  name: "Datenschutzerklaerung"
 });
 </script>
 
